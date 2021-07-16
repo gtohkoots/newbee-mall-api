0f893e4c50f06111c1c056187fb65862b0b73a63
gtohkoots <kt9sh@virginia.edu>
2021-07-16 04:56:00 -0400

    gcp deploy

--- src/main/java/ltd/newbee/mall/newbeemallapi/NewbeeMallApiApplication.java
+++ src/main/java/ltd/newbee/mall/newbeemallapi/NewbeeMallApiApplication.java
@@ -9,5 +9,6 @@ public class NewbeeMallApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NewbeeMallApiApplication.class, args);
 	}
+	
 
 }
--- src/main/java/ltd/newbee/mall/newbeemallapi/api/mall/NewBeeMallPersonalAPI.java
+++ src/main/java/ltd/newbee/mall/newbeemallapi/api/mall/NewBeeMallPersonalAPI.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,6 +34,12 @@ public class NewBeeMallPersonalAPI {
 	NewBeeMallUserService newBeeMallUserService;
 	
 	private static final Logger logger = LoggerFactory.getLogger(NewBeeMallPersonalAPI.class);
+	
+	@GetMapping("")
+	@ApiOperation(value = "测试")
+	public String test() {
+		return "Server Testing";
+	}
 	
 	@RequestMapping(value = "/user/login", method = RequestMethod.POST)
 	@ApiOperation(value = "登录接口", notes = "返回token")
